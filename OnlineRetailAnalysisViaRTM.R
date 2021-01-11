10ccec33bd82b3c5fc4fe01b63a692b20d4f699f
snigdhayan <snigdhayan@gmail.com>
2021-01-11 19:54:34 +0100

    Minor changes

--- OnlineRetailAnalysisViaRTM.R
+++ OnlineRetailAnalysisViaRTM.R
@@ -2,7 +2,7 @@
 library(tm)
 
 # read and preprocess data (make sure that the dataset 'online_retail.csv' is available in the working directory)
-setwd('/Users/ibatu/Documents/MyProgramsWindows/TextAnalysis/')
+setwd('/Users/ibatu/Documents/MyProgramsWindows/Repositories/TextAnalysis/')
 data <- read.csv(file = './online_retail.csv', header = TRUE)
 
 set.seed(123)
