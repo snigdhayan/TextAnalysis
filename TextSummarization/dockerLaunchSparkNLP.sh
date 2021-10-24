6ba1807246bffb9187276c211807eea934dda5d9
Snigdhayan Mahanta <snigdhayan@gmail.com>
2021-10-24 17:00:31 +0200

    Initial commit

---
+++ TextSummarization/dockerLaunchSparkNLP.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker build -f ./Dockerfile.txt -t mysparknlp .
+
+docker run -t -d -p 8888:8888 --name mysparknlp mysparknlp
+
+docker exec -it mysparknlp jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --port=8888
+
+# docker system prune -a -f
