25a0598431262fa6e41b72ede6818ed4e4e5064d
Snigdhayan Mahanta <snigdhayan@gmail.com>
2021-10-24 22:57:04 +0200

    Updated launch script and added notebook

--- TextSummarization/dockerLaunchSparkNLP.sh
+++ TextSummarization/dockerLaunchSparkNLP.sh
@@ -2,7 +2,7 @@
 
 docker build -f ./Dockerfile.txt -t mysparknlp .
 
-docker run -t -d -p 8888:8888 --name mysparknlp mysparknlp
+docker run -t -d -v /$(pwd)/TextSummarization:/TextSummarization -p 8888:8888 --name mysparknlp mysparknlp
 
 docker exec -it mysparknlp jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --port=8888
 
